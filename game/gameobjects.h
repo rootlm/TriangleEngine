604c1eb295335cdc3442f754b9a87b387ea53279
rootlm <liammillay2@gmail.com>
2018-07-15 22:54:09 -0400

    Add files via upload

--- game/gameobjects.h
+++ game/gameobjects.h
@@ -5,8 +5,12 @@
 #ifndef __GAMEOBJECTS__
 #define __GAMEOBJECTS__
 
+#define NUMOBJECTTYPES 512
+
 void Object_AddType(void(*createfunc)(unsigned short),void(*stepfunc)(unsigned short),void(*drawfunc)(unsigned short),unsigned short type);
 
 void Object_Init();
+
+void Object_Close();
 
 #endif
